8068acf0f46259f56d9c686022040094f4484160
DanilaBeseda <danila.beseda1999@gmail.com>
2023-10-14 13:15:37 +0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div>app</div>;
-}
+import { Box } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default App;
+import "./app.css";
+
+const theme = createTheme({
+  palette: {},
+});
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <Box>header</Box>
+        <Box sx={{ flexGrow: 1, display: "flex" }}>
+          <Box>map</Box>
+          <Box>sidebar</Box>
+        </Box>
+      </div>
+    </ThemeProvider>
+  );
+};
