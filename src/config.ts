41dbe149a15dc4b325503ac76ffff7cfe6636ae6
DanilaBeseda <danila.beseda1999@gmail.com>
2023-10-14 20:14:16 +0300

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const HOST = "26.202.5.14:7273";
+export const HOST = "http://26.202.5.14:7273";
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /* server: {
-    proxy: {
-      "/atms": "http://26.202.5.14:7273",
-    },
-  }, */
 });
