a5c0e5939ad164dd09cd64409f084d84a61d9c0e
DanilaBeseda <danila.beseda1999@gmail.com>
2023-10-14 18:55:37 +0300

    fix

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -9,10 +9,8 @@ export const Popover = ({ atm, bank }: IPopoverProps) => {
   return (
     <div className="popover">
       <div className={"blockPhoto"}>
-        {atm && (
-          <img src={"../../../public/ATM-photo.jpg"} className={"photo"} />
-        )}
-        {bank && <img src={"../../../public/Dep.jpg"} className={"photo"} />}
+        {atm && <img src={"public/ATM-photo.jpg"} className={"photo"} />}
+        {bank && <img src={"public/Dep.jpg"} className={"photo"} />}
       </div>
     </div>
   );
