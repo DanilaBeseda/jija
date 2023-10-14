c9ae9f9d1468bd3fda743ed3c1ddc7ae7413dd82
IvanZakharov11 <le_ming@mail.ru>
2023-10-14 15:48:35 +0300

    add coords

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,46 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import "./app.css";
 import Header from "./components/Header/Header.tsx";
+import {useEffect, useState} from "react";
 
 const theme = createTheme({
   palette: {},
 });
 
+
+
 export const App = () => {
+  const [lat, setLat] = useState<number>(0)
+  const [long, setLong] = useState<number>(0)
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  function success(pos: GeolocationPosition) {
+    console.log(pos.coords)
+    setLat(pos.coords.latitude)
+    setLong(pos.coords.longitude)
+    console.log("Your current position is:");
+    console.log(`Latitude : ${pos.coords.latitude}`);
+    console.log(`Longitude: ${pos.coords.longitude}`);
+    console.log(`More or less meters.`);
+  }
+
+  function error() {
+    console.warn(`ERROR`);
+  }
+
+  useEffect(() => {
+      navigator.geolocation.getCurrentPosition((pos) => success(pos), error, options)
+  }, [])
+
+
   return (
     <ThemeProvider theme={theme}>
       <div className="app">
-        <Header/>
+        <Header lat={lat} long={long}/>
         <Box sx={{ flexGrow: 1, display: "flex" }}>
           <Box>map</Box>
           <Box>sidebar</Box>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,16 @@
 import './Header.css'
 
-const Header = () => {
+interface iCoordsProps {
+    long: number,
+    lat: number
+
+}
+const Header = ({long, lat}:iCoordsProps) => {
     return (
         <div className={'mainHeader'}>
             <div className={'logoBlock'}><img src={'../../../public/VTB_logo-white_ru_rgb.svg'} className={'Logo'}/></div>
+            <div className={"probel"}/>
+            <div className={"mainCoords"}>Latitude: {lat};<br/> Longitude: {long}</div>
         </div>
         )
 }
